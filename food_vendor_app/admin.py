f321e90f9a4e98c76a20583a079b6ce9880aa481
ailevi <nkoliokafor@gmail.com>
2020-08-28 13:52:20 +0100

    Update admin file so that custom user model changes reflects

--- food_vendor_app/admin.py
+++ food_vendor_app/admin.py
@@ -1,3 +1,21 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# # Register your models here.
+from .models import (DaysOfOccurence, Meal, Menu, Order, OrderPayment,
+                     UserProfile)
+from .forms import UserProfileCreationForm, UserProfileChangeForm
+
+
+class UserProfileAdmin(UserAdmin):
+    add_form = UserProfileCreationForm
+    form = UserProfileChangeForm
+    model = UserProfile
+    list_display = ['first_name', 'last_name', 'email', 'phone_number']
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Meal)
+admin.site.register(DaysOfOccurence)
+admin.site.register(Menu)
+admin.site.register(Order)
+admin.site.register(OrderPayment)
