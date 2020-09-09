1d3123e333605593837ca0f1e18a8eac8aa99fb5
ailevi <nkoliokafor@gmail.com>
2020-09-09 20:19:03 +0100

    Add URL mappings for views objects

--- food_vendor_app/urls.py
+++ food_vendor_app/urls.py
@@ -1,3 +1,16 @@
-# from django.urls import path
+from django.urls import path
 
-# urlpatterns = []
+from .views.meal import MealDetail, MealList
+from .views.menu import MenuDetail, MenuList
+from .views.order import (OrderDetail, OrderList)
+from .views.user import UserRegistrationView
+
+urlpatterns = [
+    path('users/register/', UserRegistrationView.as_view()),
+    path('meals/', MealList.as_view()),
+    path('meals/<int:pk>/', MealDetail.as_view()),
+    path('menus/', MenuList.as_view()),
+    path('menus/<int:pk>/', MenuDetail.as_view()),
+    path('orders/', OrderList.as_view()),
+    path('orders/<int:pk>/', OrderDetail.as_view()),
+]
