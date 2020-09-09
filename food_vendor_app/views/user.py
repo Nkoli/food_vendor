e609335f04abd9a22cb3032b334b84ffae8e4a5a
ailevi <nkoliokafor@gmail.com>
2020-09-09 20:20:31 +0100

    Remove unused import

--- food_vendor_app/views/user.py
+++ food_vendor_app/views/user.py
@@ -2,7 +2,6 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from ..models import User
 from ..serializers import UserSerializer
 
 
