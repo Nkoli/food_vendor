aa7975d12c77b05f758973dbc9f5d5d475f91a49
ailevi <nkoliokafor@gmail.com>
2020-09-09 20:19:24 +0100

    Add create method to user serializer for registering a new user

--- food_vendor_app/serializers.py
+++ food_vendor_app/serializers.py
@@ -1,15 +1,25 @@
 from rest_framework import serializers
-from .models import User, Meal, Menu, Order, OrderPayment
+
+from .models import Meal, Menu, Order, OrderPayment, User
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email', 'phone_number', 'password']
+        fields = ['username', 'email', 'phone_number', 'password', ]
         extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
 
 
 class MealSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Meal
         fields = '__all__'
