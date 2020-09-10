a5ce78c40c7dc1456db0f2d813268357fde43ac5
ailevi <nkoliokafor@gmail.com>
2020-09-10 14:15:19 +0100

    Add URL mappings for token generation and refresh

--- food_vendor_app/urls.py
+++ food_vendor_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .views.meal import MealDetail, MealList
 from .views.menu import MenuDetail, MenuList
@@ -7,6 +8,8 @@ from .views.user import UserRegistrationView
 
 urlpatterns = [
     path('users/register/', UserRegistrationView.as_view()),
+    path('users/token/obtain/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('meals/', MealList.as_view()),
     path('meals/<int:pk>/', MealDetail.as_view()),
     path('menus/', MenuList.as_view()),
