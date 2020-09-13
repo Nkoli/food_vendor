43f2fefb110131669a6e56071bdf060a01bfd84e
ailevi <nkoliokafor@gmail.com>
2020-09-13 18:45:15 +0100

    Create new viewsets with custom actions for registering, logging the user in and signing out an authenticated user

--- food_vendor_app/views/user.py
+++ food_vendor_app/views/user.py
@@ -1,15 +1,74 @@
-from rest_framework import status
+import datetime
+
+from django.contrib.auth import logout
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
-from ..serializers import UserSerializer
+from ..serializers import UserLoginSerializer, UserRegisterSerializer
+from ..utils import create_user_account, get_and_authenticate_user
 
 
-class UserRegistrationView(APIView):
-    def post(self, request, format='json'):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            if user:
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+def get_tokens_for_user(user_object):
+    refresh = RefreshToken.for_user(user_object)
+    access_token = refresh.access_token
+    access_token.set_exp(lifetime=datetime.timedelta(minutes=500))
+    return {
+        'refresh': str(refresh),
+        'access': str(access_token)
+    }
+
+
+class UserRegisterViewSet(viewsets.GenericViewSet):
+    permission_classes = [AllowAny]
+    serializer_class = UserRegisterSerializer
+
+    @action(methods=['POST', ], detail=False)
+    def register(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = create_user_account(**serializer.validated_data)
+        token = get_tokens_for_user(user)
+        data = UserRegisterSerializer(user).data
+        return Response(
+            {
+                "data": data,
+                "token": token
+            },
+            status=status.HTTP_201_CREATED
+        )
+
+
+class UserLoginViewSet(viewsets.GenericViewSet):
+    permission_classes = [AllowAny]
+    serializer_class = UserLoginSerializer
+
+    @action(methods=['POST', ], detail=False)
+    def login(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = get_and_authenticate_user(**serializer.validated_data)
+        token = get_tokens_for_user(user)
+        data = UserLoginSerializer(user).data
+        return Response(
+            {
+                "data": data,
+                "token": token
+            },
+            status=status.HTTP_200_OK
+        )
+
+
+class UserLogoutViewSet(viewsets.GenericViewSet):
+    permission_classes = [IsAuthenticated]
+
+    @action(methods=['POST', ], detail=False)
+    def logout(self, request):
+        logout(request)
+        return Response(
+            {
+                "message": "Successfully logged out"
+            }, status=status.HTTP_200_OK
+        )
