5a13800c08358b7ee4a468f02fdaf30ee3424126
ailevi <nkoliokafor@gmail.com>
2020-09-13 18:43:59 +0100

    Rename register serializer, add new fields and create new login serializer

--- food_vendor_app/serializers.py
+++ food_vendor_app/serializers.py
@@ -3,10 +3,18 @@ from rest_framework import serializers
 from .models import Meal, Menu, Order, OrderPayment, User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserRegisterSerializer(serializers.ModelSerializer):
+
+    def validate(self, data):
+        is_vendor = data.get('is_vendor')
+
+        if is_vendor and not data.get('business_name'):
+            raise serializers.ValidationError('Business name is required')
+        return data
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'phone_number', 'password', ]
+        fields = ['id', 'name', 'email', 'phone_number', 'password', 'is_vendor', 'business_name']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
@@ -16,6 +24,11 @@ class UserSerializer(serializers.ModelSerializer):
             instance.set_password(password)
         instance.save()
         return instance
+
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=200, required=True)
+    password = serializers.CharField(required=True, write_only=True)
 
 
 class MealSerializer(serializers.ModelSerializer):
