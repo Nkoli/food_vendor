73ba11199bad20a7f8951bb8e70b9a4402341525
ailevi <nkoliokafor@gmail.com>
2020-09-14 19:50:36 +0100

    Remove unused import

--- food_vendor_app/views/meal.py
+++ food_vendor_app/views/meal.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-from ..models import Meal, User
+from ..models import Meal
 from ..serializers import MealSerializer
 
 
