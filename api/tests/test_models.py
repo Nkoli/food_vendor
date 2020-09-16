5b8be96ce940bd4dbe30ccb48c1497c250cec5e7
ailevi <nkoliokafor@gmail.com>
2020-09-16 20:20:01 +0100

    Create BaseTestCase with client and models, create assertions test file to check that model objects are being created

---
+++ api/tests/test_models.py
@@ -0,0 +1,33 @@
+from ..models import Days_Of_Occurence, Meal, Menu, Order, OrderPayment, User
+from .base import BaseTestCase
+
+
+class TestCreateModels(BaseTestCase):
+
+    def test_user_is_created(self):
+        self.assertEqual(User.objects.count(), 2)
+        self.assertEqual(self.test_user.phone_number, 1234567)
+
+    def test_days_of_occurence_is_created(self):
+        self.assertEqual(Days_Of_Occurence.objects.count(), 1)
+        self.assertEqual(self.days_of_occurence.days_of_occurence, 'test day')
+
+    def test_meal_is_created(self):
+        self.assertEqual(Meal.objects.count(), 1)
+        self.assertTrue(hasattr(self.meal, 'vendor'))
+        self.assertTrue(self.meal.name, 'test meal')
+
+    def test_menu_is_created(self):
+        self.assertEqual(Menu.objects.count(), 1)
+        self.assertTrue(hasattr(self.menu, 'meals'))
+        self.assertTrue(self.menu.days_of_occurence, 1)
+
+    def test_order_is_created(self):
+        self.assertEqual(Order.objects.count(), 1)
+        self.assertTrue(hasattr(self.order, 'meal'))
+        self.assertTrue(hasattr(self.order, 'menu'))
+
+    def test_order_payment_is_created(self):
+        self.assertEqual(OrderPayment.objects.count(), 1)
+        self.assertTrue(hasattr(self.order_payment, 'order'))
+        self.assertNotEqual(self.order_payment.amount_due, 11)
