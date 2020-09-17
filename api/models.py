a05cd3dc1cd7b622fccead578d7b3d04781e3ca3
ailevi <nkoliokafor@gmail.com>
2020-09-17 19:47:17 +0100

    create duplicate model and serializers data

--- api/models.py
+++ api/models.py
@@ -96,3 +96,96 @@ class OrderPayment(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_payment')
     amount_due = models.DecimalField(max_digits=8, decimal_places=2)
     amount_paid = models.DecimalField(max_digits=8, decimal_places=2)
+
+
+class User(AbstractUser):
+
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    phone_number = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    name = models.CharField(max_length=200, blank=False, null=False)
+    is_vendor = models.BooleanField(default=False)
+    business_name = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.email} - {self.name}'
+
+
+class Meal(models.Model):
+    name = models.CharField(max_length=300)
+    vendor = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+    metadata = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name} made by {self.vendor.name}'
+
+
+class Days_Of_Occurence(models.Model):
+    DAYS = [
+        ('MONDAY', 'Monday'),
+        ('TUESDAY', 'Tuesday'),
+        ('WEDNESDAY', 'Wednesday'),
+        ('THURSDAY', 'Thursday'),
+        ('FRIDAY', 'Friday'),
+        ('SATURDAY', 'Saturday'),
+        ('SUNDAY', 'Sunday')
+    ]
+
+    days_of_occurence = models.CharField(
+        max_length=15, choices=DAYS, unique=True)
+
+    def __str__(self):
+        return self.days_of_occurence
+
+
+class Menu(models.Model):
+    DIETARY_TYPE = [
+        ('VEGAN', 'Vegan'),
+        ('VEGETARIAN', 'Vegetarian'),
+        ('NON-VEGAN', 'Non-Vegan'),
+        ('GLUTEN-FREE', 'Gluten-Free'),
+        ('DAIRY-FREE', 'Dairy-Free')
+    ]
+
+    name = models.CharField(max_length=200)
+    meals = models.ManyToManyField(Meal, related_name='meals')
+    vendor = models.ForeignKey(User, on_delete=models.CASCADE)
+    dietary_type = models.CharField(max_length=20, choices=DIETARY_TYPE)
+    description = models.TextField(null=True)
+    days_of_occurence = models.ManyToManyField(Days_Of_Occurence, related_name='days')
+    frequency_of_occurence = models.IntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name}, a {self.dietary_type} menu.'
+
+
+class Order(models.Model):
+    ORDER_STATUS = [
+        ('PENDING_FULLY_PAID', 'pending_fully_paid'),
+        ('DELIVERED_FULLY_PAID', 'delivered_fully_paid'),
+        ('PENDING', 'pending'),
+        ('DELIVERED', 'delivered')
+    ]
+
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='customer_user')
+    vendor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='vendor_user')
+    status = models.CharField(max_length=25, choices=ORDER_STATUS)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, null=True, blank=True)
+    meal = models.ManyToManyField(Meal, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class OrderPayment(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_payment')
+    amount_due = models.DecimalField(max_digits=8, decimal_places=2)
+    amount_paid = models.DecimalField(max_digits=8, decimal_places=2)
--- api/serializers.py
+++ api/serializers.py
@@ -118,3 +118,120 @@ class OrderSerializer(serializers.ModelSerializer):
             payment.save()
 
         return instance
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+
+    def validate(self, data):
+        is_vendor = data.get('is_vendor')
+
+        if is_vendor and not data.get('business_name'):
+            raise serializers.ValidationError('Business name is required')
+        return data
+
+    class Meta:
+        model = User
+        fields = ['id', 'name', 'email', 'phone_number', 'password', 'is_vendor', 'business_name']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=200, required=True)
+    password = serializers.CharField(required=True, write_only=True)
+
+
+class MealSerializer(serializers.ModelSerializer):
+    vendor = serializers.ReadOnlyField(source='vendor.name')
+
+    class Meta:
+        model = Meal
+        fields = ['id', 'name', 'vendor', 'description', 'metadata']
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.metadata = validated_data.get('metadata', instance.metadata)
+        instance.save()
+        return instance
+
+
+class MenuSerializer(serializers.ModelSerializer):
+    vendor = serializers.ReadOnlyField(source='vendor.name')
+    frequency_of_occurence = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Menu
+        fields = ['id', 'name', 'meals', 'vendor', 'dietary_type',
+                  'description', 'days_of_occurence', 'frequency_of_occurence']
+
+    def update(self, instance, validated_data):
+        meals = validated_data.get('meals')
+        if meals:
+            instance.meals.add(*meals)
+
+        days_of_occurence = validated_data.get('days_of_occurence')
+        if days_of_occurence:
+            instance.days_of_occurence.add(*days_of_occurence)
+
+        instance.name = validated_data.get('name', instance.name)
+        instance.dietary_type = validated_data.get('dietary_type', instance.dietary_type)
+        instance.description = validated_data.get('description', instance.description)
+        instance.save()
+        return instance
+
+    def get_frequency_of_occurence(self, instance):
+        return instance.days_of_occurence.count()
+
+
+class OrderPaymentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = OrderPayment
+        fields = ['id', 'amount_due', 'amount_paid']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_payment = OrderPaymentSerializer(many=True)
+    customer = serializers.ReadOnlyField(source='customer.name')
+
+    class Meta:
+        model = Order
+        fields = ['id', 'customer', 'vendor', 'order_payment', 'status', 'menu', 'meal']
+
+    def create(self, validated_data):
+        meal = validated_data.pop('meal')
+        payment_details = validated_data.pop('order_payment')
+        order = Order.objects.create(**validated_data)
+        for ml in meal:
+            order.meal.add(ml)
+        for payment_detail in payment_details:
+            OrderPayment.objects.create(order=order, **payment_detail)
+        return order
+
+    def update(self, instance, validated_data):
+        payment_details = validated_data.pop('order_payment')
+        payments = (instance.order_payment).all()
+        payments = list(payments)
+        meal = validated_data.get('meal')
+        if meal:
+            instance.meal.add(*meal)
+        instance.vendor = validated_data.get('vendor', instance.vendor)
+        instance.status = validated_data.get('status', instance.status)
+        instance.menu = validated_data.get('menu', instance.menu)
+        instance.save()
+
+        for payment_detail in payment_details:
+            payment = payments.pop(0)
+            payment.amount_due = payment_detail.get('amount_due', payment.amount_due)
+            payment.amount_paid = payment_detail.get('amount_paid', payment.amount_paid)
+            payment.save()
+
+        return instance
