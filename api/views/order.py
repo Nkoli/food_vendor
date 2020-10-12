56351ed3507a32ab1598ea7e482fb2bc700b14c2
ailevi <nkoliokafor@gmail.com>
2020-10-12 16:13:16 +0100

    Switch order viewsets to use apiviews instead

--- api/views/order.py
+++ api/views/order.py
@@ -1,15 +1,52 @@
-from rest_framework import viewsets
+from django.http import Http404
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from ..models import Order
-from ..serializers import OrderSerializer
+from ..models import Order, OrderPayment
+from ..serializers import OrderPaymentSerializer, OrderSerializer
 
 
-class OrderViewSet(viewsets.ModelViewSet):
+class OrderList(APIView):
     permission_classes = [IsAuthenticated]
 
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
+    def get(self, request):
+        orders = Order.objects.all()
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data)
 
-    def perform_create(self, serializer):
-        serializer.save(customer=self.request.user)
+    def post(self, request):
+        serializer = OrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(customer=self.request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class OrderDetail(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk):
+        try:
+            return Order.objects.get(pk=pk)
+        except Order.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        order = self.get_object(pk)
+        serializer = OrderSerializer(order)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        order = self.get_object(pk)
+        serializer = OrderSerializer(order, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        order = self.get_object(pk)
+        order.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
