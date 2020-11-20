0ff72e571a323264f7e823116a65c2f0c1f70c57
ailevi <nkoliokafor@gmail.com>
2020-11-20 15:59:25 +0100

    edit files

--- food_vendor/settings/__init__.py
+++ food_vendor/settings/__init__.py
@@ -9,7 +9,7 @@ load_dotenv()
 # Ensure development settings are not used in testing and production:
 if os.getenv('ENVIRONMENT') == 'PRODUCTION':
     from .production import *
-elif os.getenv('ENVIRONMENT') == 'GITHUB':
+elif os.getenv('ENVIRONMENT') == 'GITHUB_WORKFLOW':
     from .test import *
 else:
     from .local import *
